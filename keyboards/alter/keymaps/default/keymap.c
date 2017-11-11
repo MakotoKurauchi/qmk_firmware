975b685dfffafdc228ef1f92bbd05bc2a36f912f
MakotoKurauchi <pluis@me.com>
2017-11-11 13:49:19 +0900

    change default keymap

--- keyboards/alter/keymaps/default/keymap.c
+++ keyboards/alter/keymaps/default/keymap.c
@@ -35,17 +35,6 @@ enum alter_keycodes {
   DANCE,
 };
 
-//Tap Dance Declarations
-enum {
-  TD_ESC_CAPS = 0
-};
-
-//Tap Dance Definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
-  //Tap once for Esc, twice for Caps Lock
-  [TD_ESC_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS)
-// Other declarations would go here, separated by commas, if you have them
-};
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -73,14 +62,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  | Shift|
  * `------+------+------+------+------+------+-------------+------+------+------+------+------+------'
- *        |Adjust|Esc/CA| GUI  | Alt  |Lower |Enter |Space |Raise | Left | Down |  Up  |Right |
+ *        |Adjust| Esc  | GUI  | Alt  |Lower |Enter |Space |Raise | Left | Down |  Up  |Right |
  *        `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
   KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC , \
-  MO(_ADJUST),TD(TD_ESC_CAPS),KC_LGUI, KC_LALT, MO(_LOWER),KC_ENT, KC_SPC, MO(_RAISE),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  MO(_ADJUST),KC_ESC,KC_LGUI, KC_LALT, MO(_LOWER),KC_ENT, KC_SPC, MO(_RAISE),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Lower
